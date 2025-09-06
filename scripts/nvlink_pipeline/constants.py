fix: maintain attention mask and other states needed across the stages to generate correct response

scripts/nvlink_pipeline/constants.py
@@ -2,7 +2,7 @@ import torch
 
 GB = 1024 ** 3
 MAX_LENGTH = 1024
-NUM_DEVICES = 3
+NUM_DEVICES = 1
 NUM_STAGES = 3
 DEV_GPU_ = [torch.device(f'cuda:{i % NUM_DEVICES}') for i in range(NUM_STAGES)]
 DEV_CPU = torch.device('cpu')

scripts/nvlink_pipeline/core/entities.py
@@ -25,7 +25,7 @@ class SubModel(Observable):
         self.stage_index = stage
         self.device = torch.device('cpu')
         self.layers = []
-        self.first_layer_index = 0 
+        self.first_layer_index = 0
 
         # Only the first stage will have embed_tokens set
         self.embed_tokens = None
@@ -43,23 +43,26 @@ class SubModel(Observable):
 
         if self.embed_tokens:
             self.embed_tokens.to(self.device)
-        
+
         if self.norm:
             self.norm.to(self.device)
 
-    def forward(self, input_ids, attention_mask=None, use_cache=None, past_key_values=None):
-        position_ids = None
-        cache_position = None
+    def forward(self, req):
+        input_ids = req.hidden_states
+        attention_mask = req.attention_mask
+        use_cache=True # always
 
-        # First stage
         inputs_embeds = input_ids
         if self.embed_tokens is not None:
+            # First stage
             inputs_embeds = self.embed_tokens(input_ids)
 
-        if use_cache and past_key_values is None:
-            past_key_values = DynamicCache()
+        if req.cache is None:
+            raise Exception('The request does not have a cache!')
+        past_key_values = req.cache
 
-        if cache_position is None:
+        cache_position = req.cache_position
+        if req.cache_position is None:
             past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
             cache_position = torch.arange(
                 past_seen_tokens,
@@ -67,44 +70,51 @@ class SubModel(Observable):
                 device=self.device
             )
 
+        position_ids = req.position_ids
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
 
-        mask_function = create_causal_mask if self.config.sliding_window is None else create_sliding_window_causal_mask
-        causal_mask = mask_function(
-            config=self.config,
-            input_embeds=inputs_embeds,
-            attention_mask=attention_mask,
-            cache_position=cache_position,
-            past_key_values=past_key_values,
-            position_ids=position_ids,
-        )
+        causal_mask = req.causal_mask
+        if causal_mask is None:
+            mask_function = create_causal_mask if self.config.sliding_window is None else create_sliding_window_causal_mask
+            causal_mask = mask_function(
+                config=self.config,
+                input_embeds=inputs_embeds,
+                attention_mask=attention_mask,
+                cache_position=cache_position,
+                past_key_values=past_key_values,
+                position_ids=position_ids,
+            )
 
         hidden_states = inputs_embeds
-        position_embeddings = self.rotary_emb(hidden_states, position_ids)
 
-        with torch.no_grad():
-            for k, decoder_layer in enumerate(self.layers):
-                hidden_states = decoder_layer(
-                    hidden_states,
-                    attention_mask=causal_mask,
-                    position_ids=position_ids,
-                    past_key_value=past_key_values,
-                    use_cache=use_cache,
-                    cache_position=cache_position,
-                    position_embeddings=position_embeddings,
-                )
-                self._notify('layer_finish', self.first_layer_index + k)
+        position_embeddings = req.position_embeddings
+        if position_embeddings is None:
+            position_embeddings = self.rotary_emb(hidden_states, position_ids)
 
-            # Last stage
-            if self.norm is not None:
-                hidden_states = self.norm(hidden_states)
+        for k, decoder_layer in enumerate(self.layers):
+            hidden_states = decoder_layer(
+                hidden_states,
+                attention_mask=causal_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_values,
+                use_cache=use_cache,
+                cache_position=cache_position,
+                position_embeddings=position_embeddings,
+            )
+            # if there is code that wants to work layer by later ...
+            self._notify('layer_finish', self.first_layer_index + k)
 
-        out = BaseModelOutputWithPast(
-            last_hidden_state=hidden_states,
-            past_key_values=past_key_values if use_cache else None,
-        )
-        return out
+        # Last stage
+        if self.norm is not None:
+            hidden_states = self.norm(hidden_states)
+
+        # hidden request state
+        req.position_ids = position_ids
+        req.cache_position = cache_position
+        req.causal_mask = causal_mask
+        req.position_embeddings = position_embeddings
+        req.hidden_states = hidden_states
 
 
 class Replica:
@@ -119,7 +129,7 @@ class Replica:
 
         # TODO: is this not on a GPU?
         self.tokenizer = AutoTokenizer.from_pretrained(model_name,
-                                                    local_files_only=LOCAL_FILE_ONLY)
+                                            local_files_only=LOCAL_FILE_ONLY)
 
         num_layers = model.model.config.num_hidden_layers
         layers = model.model.layers[:num_layers]
@@ -159,7 +169,7 @@ class Replica:
             # input('continue? ')
         # move the lm_head to the last stage's device
         self.lm_head = self.lm_head.to(self.stages[-1].device)
-    
+
     def get_kv_cache_token_size(self) -> int:
         """
         Approximately how much memory one token of kv cache will consume
@@ -180,144 +190,50 @@ class Replica:
         """
         bytes = self.get_kv_cache_token_size() * max_length
         return bytes
-    
+
     def do_one_iteration(self, req, stats=None) -> torch.Tensor:
         """
         Do one full iteration on the request through all the stages of the pipeline.
         This will return the next token.
         The request must have gone through tokenization phase and have the next_token_ids set to the input for the first stage.
         """
-        hidden_state = req.next_token_ids
 
         with torch.no_grad():
+            req.hidden_states = req.next_token_ids
             for stage in self.stages:
                 cache = req.cache
 
-                start = time.time()
                 # bring the input/hidden state to device
-                hidden_state = hidden_state.to(stage.device, non_blocking=True)
+                start = time.time()
+                req.hidden_states = req.hidden_states.to(stage.device, non_blocking=True)
 
-                # TODO: can we do something here to use the time?
-
-                # wait for data transfer to finish
-                torch.cuda.synchronize(stage.device)
-                duration = (time.time() - start) * 1000
                 if stats:
+                    torch.cuda.synchronize(stage.device)
+                    duration = (time.time() - start) * 1000
                     stats.hidden_state_transfer_times[stage.stage_index].append(duration)
-                    sz = hidden_state.numel() * hidden_state.element_size()
+                    sz = req.hidden_states.numel() * req.hidden_states.element_size()
                     stats.hidden_state_transfer_size[stage.stage_index].append(sz)
 
                 start = time.time()
-                out = stage.forward(hidden_state, attention_mask=req.attention_mask,
-                                        use_cache=True, past_key_values=cache)
-                # wait until all computation on this device is over
-                torch.cuda.synchronize(stage.device)
-                duration = (time.time() - start) * 1000
+                stage.forward(req)
                 if stats:
+                    # wait until all computation on this device is over
+                    torch.cuda.synchronize(stage.device)
+                    duration = (time.time() - start) * 1000
                     stats.stage_exec_times[stage.stage_index].append(duration)
 
-                hidden_state = out.last_hidden_state
-
-        logits = self.lm_head(out.last_hidden_state)
+        # Select the most probable token as next toekn
+        logits = self.lm_head(req.hidden_states)
         logits = logits.float()
         next_token_logits = logits[:, -1, :]
         next_token = torch.argmax(next_token_logits, dim=-1, keepdim=True)
+
+        # Update attention to use the new token
+        mask = req.attention_mask
+        mask = torch.cat([mask, mask.new_ones((mask.size(0), 1))], dim=-1)
+        req.attention_mask = mask 
+
+        req.clear_hidden_states()
+
         return next_token
 
-
-class Request:
-    counter = 1
-
-    def __init__(self, prompt: str):
-        self.id = Request.counter
-        Request.counter += 1
-
-        # prompt string
-        self.prompt = prompt
-
-        # List of tokens
-        # NOTICE: not all tokens are on the same device!
-        self.generated: List[torch.Tensor] = []
-
-        # next token
-        self.next_token_ids: Optional[torch.Tensor] = None
-        self.attention_mask: Optional[torch.Tensor] = None
-
-        # KV Cache of each stage
-        self.cache = DynamicCache()
-
-        self._pre_move_state: Dict[int, List[Tuple[int, torch.Tensor, torch.Tensor]]] = {}
-    
-    def move_to(self, device_map, non_blocking=False) -> None:
-        """
-        Move KV cache of request to the given devices device map indicates for
-        each layer of KV-Cache which device it should go to.
-        """
-
-        cache = self.cache
-        num_layers = len(cache.layers)
-        for i in range(num_layers):
-            dev = device_map[i]
-            if dev is None or cache.layers[i].keys.device == dev:
-                # do not move this layer
-                continue
-            cache.layers[i].keys = cache.layers[i].keys.to(dev, non_blocking=non_blocking)
-            cache.layers[i].values = cache.layers[i].values.to(dev, non_blocking=non_blocking)
-    
-    def pre_move_to(self, pre_move_key, device_map, non_blocking=False) -> None:
-        """
-        Similar as move_to, but does not update the KV-Cache poitners until
-        pre_move_apply is called.
-        
-        Attention: if using non-blocking mode, make sure to call pre_move_apply
-        after pre_move_to is finished.
-        """
-
-        if pre_move_key in self._pre_move_state:
-            raise Exception('Overwirting a previous pre-move state', pre_move_key)
-
-        state: List[Tuple[int, torch.Tensor, torch.Tensor]] = []
-        self._pre_move_state[pre_move_key] = state
-        
-        for i, layer in enumerate(self.cache.layers):
-            target_dev = device_map[i]
-            if target_dev is None or layer.keys.device == target_dev:
-                # do not move this layer
-                continue
-            else:
-                new_key = layer.keys.to(target_dev, non_blocking=non_blocking)
-                new_value = layer.values.to(target_dev, non_blocking=non_blocking)
-                state.append((i, new_key, new_value))
-    
-    def apply_pre_move(self, pre_move_key):
-        state = self._pre_move_state.get(pre_move_key)
-        if state is None:
-            raise Exception('The premove state is empty. Probably calling apply before pre-moving!')
-        
-        for index, new_keys, new_values in state:
-            self.cache.layers[index].keys = new_keys
-            self.cache.layers[index].values = new_values
-
-        del self._pre_move_state[pre_move_key]
-    
-    def cache_size_bytes(self) -> int:
-        dc = self.cache
-        nbytes = 0
-        print('--->', 'num layers:', len(dc.layers), 'num floats:',
-        dc.layers[0].keys.numel(), 'shape:', dc.layers[0].keys.shape)
-        for layer in dc.layers:
-            nbytes += layer.keys.numel()   * layer.keys.element_size()
-            nbytes += layer.values.numel() * layer.values.element_size()
-        return nbytes
-    
-    def bytes(self) -> int:
-        b = self.cache_size_bytes()
-        for t in self.generated:
-            b += t.numel() * t.element_size()
-        return b 
-    
-    def free(self):
-        # TODO: Do I need to do this? why?
-        self.cache = None
-        self.generated.clear()
-        self.next_token_ids = None

scripts/nvlink_pipeline/core/pipeline/simple_pipeline.py
@@ -3,9 +3,10 @@ import time
 import torch
 from transformers.generation.utils import DynamicCache
 
-from core.entities import Request, Replica
+from core.entity import Request
+from core.entities import Replica
 from core.statistics import ExecutionStatistics
-from core.prefill_decode import do_prefill, do_batch_prefill, do_decode, print_output
+from core.prefill_decode import do_prefill, do_batch_prefill
 from utils.memman import get_batch_size, get_max_num_tokens
 from constants import *
 
@@ -24,13 +25,20 @@ class SimplePipeline:
 
         self.rx_queue: List[Request] = []
         self.run_queue: List[Request] = [] # Requests can actually be batched Rquests
-    
+
     def close(self) -> None:
         return
-    
+
+    def print_output(self, req: Request) -> None:
+        # Actually generate the text
+        generated = torch.cat([t.to(DEV_CPU) for t in req.generated], dim=-1)
+        final_text = self.replica.tokenizer.batch_decode(generated,
+                                                skip_special_tokens=True)
+        print(final_text)
+
     def add_request(self, req):
         self.rx_queue.append(req)
-    
+
     def prepare_run_queue(self) -> None:
         # Do prefill of all request in advance
         for i, req in enumerate(self.rx_queue):
@@ -39,8 +47,9 @@ class SimplePipeline:
             dev_map = [DEV_CPU] * count
             req.move_to(dev_map, non_blocking=True)
             self.run_queue.append(req)
+        self.rx_queue.clear()
         torch.cuda.synchronize()
-    
+
     def prepare_run_queue_batched(self) -> None:
         batch_size = self.batch_size
 
@@ -57,19 +66,20 @@ class SimplePipeline:
             batched_req.move_to(dev_map, non_blocking=True)
             self.run_queue.append(batched_req)
         torch.cuda.synchronize()
-    
+
     def _do_process_reqeust(self, req: Request) -> None:
         # stat = ExecutionStatistics(self.num_stages)
         stat = None
-        do_decode(req, self.replica, stat, max_iter=self.max_length)
-        print_output(req)
+        for _ in range(self.max_length):
+            next_token = self.replica.do_one_iteration(req, stat)
+            req.generated.append(next_token)
+            req.next_token_ids = next_token
 
+        self.print_output(req)
         # stat.report()
-
         print('Req', req.id, 'size:', req.bytes())
         req.free()
-        # torch.cuda.empty_cache()
-    
+
     def process_requests(self)-> None:
         if not self.run_queue:
             return
@@ -78,14 +88,10 @@ class SimplePipeline:
         count = len(self.run_queue[0].cache.layers)
         r = count // self.num_stages
         dev_map = [self.replica.stages[i // r].device for i in range(count)]
-        
+
         while self.run_queue:
             req = self.run_queue.pop()
             # move batch of requests to GPUs
             req.move_to(dev_map, non_blocking=True)
-
-            # We are loading request/batch of request into multiple GPUs
-            # wait until all is loaded
-            # torch.cuda.synchronize()
-
             self._do_process_reqeust(req)
+

scripts/nvlink_pipeline/core/pipeline/swapping_pipeline.py
@@ -3,7 +3,6 @@ import torch
 from transformers.generation.utils import DynamicCache
 
 from core.pipeline.simple_pipeline import SimplePipeline
-from core.prefill_decode import print_output
 from utils.worker import Worker, Promise
 
 
@@ -34,25 +33,25 @@ class SwappingPipeline(SimplePipeline):
         self._in_flight_copies: Dict[int, List[Promise]] = {i: [] for i in range(num_stages)}
 
         self._active_request: List[Optional[Tuple[torch.Tensor, torch.Tensor]]] = [None,] * len(self.replica.stages)
-    
+
     def __del__(self):
         self.close()
 
     def close(self) -> None:
         """
-        Close pipeline and release resources 
+        Close pipeline and release resources
         """
         for w in self._workers + self._kv_cache_workers:
             w.die()
-        
+
         self._unregister_from_layer_completion()
 
-    
+
     def _register_for_layer_completion(self):
         # Register layer completion callbacks for moving KV-cache layer by layer
         for index, stage in enumerate(self.replica.stages):
             stage.register('layer_finish', self._move_kv_cache_layer, index)
-    
+
     def _unregister_from_layer_completion(self):
         for index, stage in enumerate(self.replica.stages):
             stage.unregister('layer_finish', self._move_kv_cache_layer)
@@ -79,10 +78,11 @@ class SwappingPipeline(SimplePipeline):
         next_stage = self.replica.stages[next_stage_index]
 
         with torch.Stream(device=stage.device) as s_cuda:
-            out = stage.forward(req1.next_token_ids,
-                                attention_mask=req1.attention_mask,
-                                use_cache=True,
-                                past_key_values=req1.cache)
+            with torch.no_grad():
+                out = stage.forward(req1.next_token_ids,
+                                    attention_mask=req1.attention_mask,
+                                    use_cache=True,
+                                    past_key_values=req1.cache)
 
             # Move the activation values to the next stage
             req1.next_token_ids = out.last_hidden_state
@@ -93,7 +93,7 @@ class SwappingPipeline(SimplePipeline):
                 self._finish_req_iter(req1)
 
             req1.next_token_ids = req1.next_token_ids.to(next_stage.device, non_blocking=True)
-    
+
     def _do_move_kv_cache_layer(self, stage_index, layer_index):
         """
         This function should run in a kv-cache worker thread. So the blocking
@@ -131,7 +131,7 @@ class SwappingPipeline(SimplePipeline):
         w = self._kv_cache_workers[stage_index]
         p = w.add_task(self._do_move_kv_cache_layer, stage_index, layer_index)
         self._in_flight_copies[stage_index].append(p)
- 
+
     def swapping_decode_on_stage(self, req1, req2, stage_index, finish) -> Promise:
         """
         ATTENTION: The order of calling this function on requests and
@@ -158,7 +158,7 @@ class SwappingPipeline(SimplePipeline):
 
         # NOTE: When the worker finishes processing a layer _move_kv_cache_layer
         # is called and that layers KV cache is moved
- 
+
         def local_load_other_req():
             # wait until req1 calculation is over
             promise.wait()
@@ -175,7 +175,7 @@ class SwappingPipeline(SimplePipeline):
             # fully utilizing the GPUs
             # num_layers = len(req2.cache.layers)
             # per_layer =  int(num_layers // 2)
-            # f = stage.first_layer_index 
+            # f = stage.first_layer_index
             # t = f + per_layer
             # dev_map = [None] * num_layers
             # dev_map[f:t] = [stage.device, ] * per_layer
@@ -255,8 +255,8 @@ class SwappingPipeline(SimplePipeline):
                 y.wait()
                 # torch.cuda.synchronize()
 
-            print_output(req1)
-            print_output(req2)
+            self.print_output(req1)
+            self.print_output(req2)
 
             # stat.report()
 
@@ -267,6 +267,6 @@ class SwappingPipeline(SimplePipeline):
             req1.free()
             req2.free()
             # torch.cuda.empty_cache()
-        
+
         self._unregister_from_layer_completion()
 

scripts/nvlink_pipeline/core/prefill_decode.py
@@ -1,5 +1,8 @@
 from typing import *
-from .entities import Request, Replica
+import torch
+from core.entity import Request
+from .entities import Replica
+from constants import DEV_CPU
 
 
 def do_prefill(req, replica):
@@ -16,30 +19,6 @@ def do_prefill(req, replica):
     req.next_token_ids = next_token
     req.generated.append(req.next_token_ids)
 
-
-def do_decode(req, replica, stat, max_iter=32) -> None:
-    for _ in range(max_iter):
-        next_token = replica.do_one_iteration(req, stat)
-
-        # TODO: maybe avoid moving to CPU? or copy to CPU and other GPU async
-        # next_token = next_token.cpu()
-        # req.generated = torch.cat([req.generated, next_token], dim=-1)
-        req.generated.append(next_token)
-
-        req.next_token_ids = next_token
-
-        # TODO: what should I do when having a batched request?
-        # if next_token.item() == replica.tokenizer.eos_token_id:
-        #     break
-        
-        # torch.cuda.empty_cache()
-
-def print_output(req):
-    # Actually generate the text
-    # generated = torch.cat([t.to(DEV_CPU) for t in req.generated], dim=-1)
-    # final_text = replica.tokenizer.decode(generated[0], skip_special_tokens=True)
-    # print(final_text)
-    return
 
 def do_batch_prefill(requests: List[Request], replica: Replica) -> Request:
     import sys